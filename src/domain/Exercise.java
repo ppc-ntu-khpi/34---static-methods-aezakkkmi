8724abbd3bdb7202ece6ad9b925bdc2af531bced
aezakkkmi <72559370+aezakkkmi@users.noreply.github.com>
2021-01-30 15:02:22 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,24 @@
-package domain;
-
+    package domain;
+/**
+ * @see
+ * @author God
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+/**
+ * Це шифратор, який шифрує потрібний текст.
+ * @param text це -  заданий текст користувачем.
+ * @param keyWord це - KeyWord ключ.
+ * @return result
+ */
+    public static byte[] encrypt(String text, String keyWord)
+{
+    byte[] arr = text.getBytes();
+    byte[] keyarr = keyWord.getBytes();
+    byte[] result = new byte[arr.length];
+    for(int i = 0; i< arr.length; i++)
+    {
+        result[i] = (byte) (arr[i] ^ keyarr[i % keyarr.length]);
     }
+    return result;
+}
 }
